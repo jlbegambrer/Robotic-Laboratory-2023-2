8437922161ee50ba9809c49fe12679d4d25b0aba
carlos200103 <72324507+carlos200103@users.noreply.github.com>
2023-11-26 19:19:06 -0500

    Codigos principales
    Código de prueba de funciones básicas y código con las trayectorias usadas

--- Laboratorio5/Codigos Matlab/Lab5Logica.m
+++ Laboratorio5/Codigos Matlab/Lab5Logica.m
@@ -41,21 +41,21 @@ LEN_MX_MOV_SPEED            = 2;
 LEN_MX_PRESENT_SPEED        = 2;
 LEN_Present_Load            = 2;
 
-%%
+%% Conexión al puerto y configuración
 [port_num,lib_name,group_num] = ConnectPhantomX(DEVICENAME,BAUDRATE,PROTOCOL_VERSION,ADDR_MX_GOAL_POSITION,LEN_MX_GOAL_POSITION);
 
-%%
+%% Se habilita el Torque de cuatro motores
 EnableTorque(port_num,PROTOCOL_VERSION,DXL1_ID,1)
 EnableTorque(port_num,PROTOCOL_VERSION,DXL2_ID,1)
 EnableTorque(port_num,PROTOCOL_VERSION,DXL3_ID,1)
 EnableTorque(port_num,PROTOCOL_VERSION,DXL4_ID,1)
 
-%%
+%% Se cambia el valor del torque de cuatro motores
 ChangeTorque(port_num,PROTOCOL_VERSION,DXL1_ID,700)
 ChangeTorque(port_num,PROTOCOL_VERSION,DXL2_ID,700)
 ChangeTorque(port_num,PROTOCOL_VERSION,DXL3_ID,700)
 ChangeTorque(port_num,PROTOCOL_VERSION,DXL4_ID,700)
-%%
+%% Se obtiene el valor en bits de cuatro posiciones angulares
 pos1 = 0;%80;
 pos2 = 0;%-35;
 pos3 = 0;%55;
@@ -66,89 +66,127 @@ pos2B = Angle2Bit(0,pos2);
 pos3B = Angle2Bit(-90,pos3);
 pos4B = Angle2Bit(0,pos4);
 
-%%
-Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,12)
+%% Se mueve un motor a una posición en bits
+Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,512)
 
-%%
+%% Se mueven dos motores a una posición en bits
 Move2Motors(port_num,PROTOCOL_VERSION,group_num,1.5,DXL1_ID,512,DXL2_ID,pos2B)
 
-%%
+%% Se mueven cuatro motores a una posición en bits
 Move4Motors(port_num,PROTOCOL_VERSION,group_num,1.5,pos1B,pos2B,pos3B,pos4B)
 
-%%
+%% Se cambia el valor de una dirección en memoria especifica (Moving speed)
 WriteAddrValue(port_num,PROTOCOL_VERSION,DXL4_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,350)
 
-%%
+%% Se lee el valor de una dirección de memoria especifica (Moving speed)
 ReadAddrValue(port_num,PROTOCOL_VERSION,DXL1_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED)
 
-%%
+%% Se cambia el valor de una dirección en memoria especifica (Goal position)
 WriteAddrValue(port_num,PROTOCOL_VERSION,DXL1_ID,ADDR_MX_GOAL_POSITION,LEN_MX_GOAL_POSITION,512)
 
-%%
+%% Se lee el valor de una dirección de memoria especifica (Goal position)
 ReadAddrValue(port_num,PROTOCOL_VERSION,DXL1_ID,ADDR_MX_PRESENT_POSITION,LEN_MX_PRESENT_POSITION)
 ReadAddrValue(port_num,PROTOCOL_VERSION,DXL2_ID,ADDR_MX_PRESENT_POSITION,LEN_MX_PRESENT_POSITION)
 ReadAddrValue(port_num,PROTOCOL_VERSION,DXL3_ID,ADDR_MX_PRESENT_POSITION,LEN_MX_PRESENT_POSITION)
 ReadAddrValue(port_num,PROTOCOL_VERSION,DXL4_ID,ADDR_MX_PRESENT_POSITION,LEN_MX_PRESENT_POSITION)
-%%
+%% Se mueve el robot a una de las poses del Lab4 y se obtiene el valor real de los ángulos
 [pos1,pos2,pos3,pos4]=Mov2Pose(port_num,PROTOCOL_VERSION,group_num,2.5,2);
 
-%%
+%% Se escriben simultaneamente cuatro valores en la dirección especifica (goal position)
 DXL_ID = [1 2 3 4];
 dxl_goal_position = [pos1B pos2B pos3B pos4B];
 SyncWrite(port_num,PROTOCOL_VERSION,DXL_ID,dxl_goal_position,ADDR_MX_GOAL_POSITION,LEN_MX_GOAL_POSITION)
 
-%%
+%% Se devueleve el robot a la posición de Home a una velocidad especifica
 Home(port_num,PROTOCOL_VERSION,50);
 
-%%
-%P = [29 0 12];
+%% Se mueve el robot a un punto usando solo los valores finales de la cinematica inversa
 P = [23 3 12];
 Theta = 0;
 speed = 100;
 MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
 
-%%
-P = [25 0 12];
+%% Se mueve el robot en linea recta aproximando los puntos en divisiones iguales de x, y y z
+Pin = P;
+P = [18 0 12];
 Theta = 0;
 speed = 230;
-Ppath = MovLLinspace(port_num,PROTOCOL_VERSION,P,Theta,speed,0.01,0.01);
-figure
-plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
-grid on
+Ppath = MovLLinspace(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.01);
+% figure
+% plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
+% grid on
+disp(Ppath)
 
-%%
-%P = [25 0 12];
-P = [23 -2 12];
+%% Se mueve el robot en linea recta aproximando los puntos en segmentos iguales
+Pin = P;
+P = [22 7 11.5];
 Theta = 0;
-speed = 150;
-Ppath = MovL(port_num,PROTOCOL_VERSION,P,Theta,speed,0.001,0.001);
-figure
-plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
-grid on
+speed = 50;
+Ppath = MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+% figure
+% plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
+% grid on
+disp(Ppath)
 
-%%
-R = 4;
-cuadrante = 3;
-concavo = 0;
+%% Se elaboran cuartos de circunferencia en el plano xy según especificaciones
+Pin = P;
+R = 2;
+cuadrante = 2;
+concavo = 1;
 Theta = 0;
-speed = 100;
-Ppath = MovSCxy(port_num,PROTOCOL_VERSION,R,cuadrante,concavo,Theta,speed,0.001,0.01);
-figure
-plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
-grid on
-view(30,30)
+speed = 50;
+[P,Ppath] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+% figure
+% plot3(Ppath(:,1),Ppath(:,2),Ppath(:,3),'Marker','o')
+% grid on
+% view(30,30)
 
-%%
+%% Ciclo para mover el robot linealmente en x, y y z con teclado numerico
+while 1
+    disp(P)
+    in = input('Ingrese la Instrucción: ');
+    Pin = P;
+    Theta = 0;
+    speed = 50;
+    switch in
+        case 5
+            P = [P(1)+1 P(2) P(3)];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 2
+            P = [P(1)-1 P(2) P(3)];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 3
+            P = [P(1) P(2)-1 P(3)];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 1
+            P = [P(1) P(2)+1 P(3)];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 9
+            speed = 20;
+            P = [P(1) P(2) P(3)+0.5];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 6
+            speed = 20;
+            P = [P(1) P(2) P(3)-0.5];
+            MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+        case 1234
+            break
+        otherwise
+            disp('Instrucción incorrecta')
+    end
+end
+
+%% Obtención de la posición leyendo motores y usando cinematica directa
 [q, P, Theta] = ActualPosition(port_num,PROTOCOL_VERSION);
 
-%%
+%% Se abre y se cierra el Gripper a un valor con cierta velocidad
 %Abierto = 820
 %Cerrado = <512
 Gripper(port_num,PROTOCOL_VERSION,2,840,500)
 
-%%
+%% Lectura del registro de errores de los motores
 %REGISTRO ERRORES
 dec2bin(ReadAddrValue(port_num,PROTOCOL_VERSION,DXL2_ID,17,1))
 
-%%
+%% Desconexión y liberación del puerto
 DisconnectPhantomX(port_num,lib_name)
--- Laboratorio5/Codigos Matlab/Lab5Planificacion.m
+++ Laboratorio5/Codigos Matlab/Lab5Planificacion.m
@@ -1,54 +1,481 @@
 %% Planificación
-%
+% Ya esta todo conectado y las variables de la Adress Table declaradas
+
 %% Se envia a HOME
-Home(port_num,PROTOCOL_VERSION,100);
+Home(port_num,PROTOCOL_VERSION,70);
+
+%% Se aproxima al marcador
+P = [9 -22 22];
+Theta = 0;
+speed = 100;
+MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
+
+%% Baja al marcador
+P = [9 -22 12];
+Theta = 0;
+speed = 20;
+MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
 
 %% Se abre y se cierra el gripper
 %Abierto = 820
-Gripper(port_num,PROTOCOL_VERSION,2,820,500)
-%Cerrado = <512
-Gripper(port_num,PROTOCOL_VERSION,2,510,500)
+Gripper(port_num,PROTOCOL_VERSION,2,520,300)
+%% Cerrado = <512
+Gripper(port_num,PROTOCOL_VERSION, ...
+    2,820,300)
 
-%% Se ubica en el limite a cierta altura
-P = [18 0 20];
+%% Se aleja del porta
+Pin = P;
+P = [9 -22 24];
+Theta = 0;
+
+speed = 100;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0000001);
+
+%% Se envia a HOME
+Home(port_num,PROTOCOL_VERSION,70);
+
+%% Se ubica en el limite inferior a cierta altura
+P = [17 0 17];
 Theta = 0;
 speed = 100;
 MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
 
 %% Se baja el robot
-P = [18 0 15];
+P = [17 0 14];
+
 Theta = 0;
-speed = 100;
+speed = 50;
 MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
 
 %% Se aproxima al tablero
-P = [18 0 11];
+P = [17 0 11];
 Theta = 0;
-speed = 100;
+speed = 20;
 MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
 
 %% Dibuja el arco inicial
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL1_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL2_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL3_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL4_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+%pause(3)
 pos1B = Angle2Bit(0,0);
 Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos1B)
-pos2B = Angle2Bit(0,30);
+pause(0.5)
+pos2B = Angle2Bit(0,40);
 Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos2B)
-pos3B = Angle2Bit(0,-30);
+pause(2)
+pos3B = Angle2Bit(0,-40);
 Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos3B)
+pause(5)
 pos1B = Angle2Bit(0,0);
 Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos1B)
 
 %% Se aleja del tablero
 P = [21 0 17];
 Theta = 0;
-speed = 100;
+speed = 50;
 MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
-%%
-P = [28 0 17];
+
+%% Se ubica en el limite superior a cierta altura
+Pin = P;
+P = [30 0 17];
 Theta = 0;
 speed = 100;
-MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
-%%
-P = [28 -4 12];
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.01,0.0001)
+
+%% Se baja el robot
+P = [30 0 14];
 Theta = 0;
-speed = 150;
-Ppath = MovL(port_num,PROTOCOL_VERSION,P,Theta,speed,0.001,0.001);
+speed = 20;
+MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
+
+%% Se aproxima al tablero
+P = [30 0 12];
+Theta = 0;
+speed = 5;
+MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
+
+%% Dibuja el arco final
+
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL1_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL2_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL3_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+WriteAddrValue(port_num,PROTOCOL_VERSION,DXL4_ID,ADDR_MX_MOV_SPEED,LEN_MX_MOV_SPEED,100)
+%pause(3)
+pos1B = Angle2Bit(0,0);
+Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos1B)
+pos2B = Angle2Bit(0,30);
+Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos2B)
+pause(4)
+pos3B = Angle2Bit(0,-30);
+Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos3B)
+pause(10)
+pos1B = Angle2Bit(0,0);
+Move1Motor(port_num,PROTOCOL_VERSION,1.5,DXL1_ID,pos1B)
+
+%% Alejo del tablero
+P = [23 0 17];
+Theta = 0;
+speed = 20;
+MovJ(port_num,PROTOCOL_VERSION,P,Theta,speed)
+
+%% Se envia a HOME
+Home(port_num,PROTOCOL_VERSION,70);
+
+%% CJ
+%PUNTO INICIAL ALCANZADO
+
+%% Curva 1
+Pin = P;
+R = 4;
+cuadrante = 4;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 2
+Pin = P;
+R = 4;
+cuadrante = 3;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Linea 1
+Pin = P;
+P = [P(1)+2 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+
+%% Curva 3
+Pin = P;
+R = 2;
+cuadrante = 3;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 4
+Pin = P;
+R = 2;
+cuadrante = 2;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Linea 2
+Pin = P;
+P = [P(1)+6 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+
+%% VY
+%PUNTO INICIAL ALCANZADO
+
+%% Linea 1
+Pin = P;
+P = [P(1)-6 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 2
+Pin = P;
+P = [P(1)+6 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 3
+Pin = P;
+P = [P(1)-4 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 4
+Pin = P;
+P = [P(1)+4 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 5
+Pin = P;
+P = [P(1)-4 P(2)+2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 6
+Pin = P;
+P = [P(1)-2 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+
+%% JML
+%PUNTO INICIAL ALCANZADO
+%% Curva 1
+Pin = P;
+R = 2;
+cuadrante = 3;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 2
+Pin = P;
+R = 2;
+cuadrante = 2;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Linea 1
+Pin = P;
+P = [P(1)+4 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 2
+Pin = P;
+P = [P(1)-2 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 3
+Pin = P;
+P = [P(1)+2 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 4
+Pin = P;
+P = [P(1)-6 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 5
+Pin = P;
+P = [P(1) P(2)-4 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+
+%% <3
+%PUNTO INICIAL ALCANZADO
+%% Curva 1
+Pin = P;
+R = 3;
+cuadrante = 2;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 2
+Pin = P;
+R = 3;
+cuadrante = 3;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 3
+Pin = P;
+R = 3;
+cuadrante = 2;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 4
+Pin = P;
+R = 3;
+cuadrante = 3;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Linea 1
+Pin = P;
+P = [P(1)-4 P(2)+6 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 2
+Pin = P;
+P = [P(1)+4 P(2)+6 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+
+%% -oo-
+%PUNTO INICIAL ALCANZADO
+%% Linea 1
+Pin = P;
+P = [P(1) P(2)-3 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Curva 1
+Pin = P;
+R = 3;
+cuadrante = 2;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 2
+Pin = P;
+R = 3;
+cuadrante = 3;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 3
+Pin = P;
+R = 3;
+cuadrante = 2;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 4
+Pin = P;
+R = 3;
+cuadrante = 3;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Linea 2
+Pin = P;
+P = [P(1) P(2)-3 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 1
+Pin = P;
+P = [P(1) P(2)+3 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Curva 5
+Pin = P;
+R = 3;
+cuadrante = 4;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 6
+Pin = P;
+R = 3;
+cuadrante = 1;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 7
+Pin = P;
+R = 3;
+cuadrante = 4;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 8
+Pin = P;
+R = 3;
+cuadrante = 1;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+
+%% Hojita
+%PUNTO INICIAL ALCANZADO
+%% Linea 1
+Pin = P;
+P = [P(1)-2 P(2)+2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 2
+Pin = P;
+P = [P(1)-2 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 3
+Pin = P;
+P = [P(1)+2 P(2)-2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 4
+Pin = P;
+P = [P(1)+2 P(2)+2 P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Linea 5
+Pin = P;
+P = [P(1)+2 P(2) P(3)];
+Theta = 0;
+speed = 50;
+MovL(port_num,PROTOCOL_VERSION,Pin,P,Theta,speed,0.005,0.0001);
+%% Curva 1
+Pin = P;
+R = 4;
+cuadrante = 1;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 2
+Pin = P;
+R = 4;
+cuadrante = 3;
+concavo = 1;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 3
+Pin = P;
+R = 4;
+cuadrante = 2;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
+%% Curva 4
+Pin = P;
+R = 4;
+cuadrante = 4;
+concavo = 0;
+Theta = 0;
+speed = 50;
+[P,~] = MovSCxy(port_num,PROTOCOL_VERSION,Pin,R,cuadrante,concavo,Theta,speed,0.005,0.0001);
+disp(P)
